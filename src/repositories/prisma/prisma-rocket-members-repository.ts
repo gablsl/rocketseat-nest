782c046537e22be16cb8aca8001f9fdc1025acb1
gablsl <gabrielsousadeveloper@gmail.com>
2024-08-14 00:33:41 -0300

    feat: first commit

---
+++ src/repositories/prisma/prisma-rocket-members-repository.ts
@@ -0,0 +1,19 @@
+import { PrismaService } from './../../database/prisma.service';
+import { RocketMembersRepository } from '../rocket-member-repository';
+import { randomUUID } from 'node:crypto';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PrismaRocketMembersRepository implements RocketMembersRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(name: string, memberFunction: string): Promise<void> {
+    await this.prisma.rocketTeamMember.create({
+      data: {
+        id: randomUUID(),
+        name,
+        function: memberFunction,
+      },
+    });
+  }
+}
